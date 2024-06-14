284c258614716b2782d81b318c7656077bbefb27
kaanyinaele <kaanyinaele@gmail.com>
2024-06-14 16:04:47 +0100

    Time for interview

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Python script that lists 10 commits (from the most recent to oldest)
+of a given repository by a given user using the GitHub API.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    repository = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = f"https://api.github.com/repos/{owner}/{repository}/commits"
+    response = requests.get(url)
+
+    commits = response.json()
+
+    for commit in commits[:10]:
+        sha = commit.get('sha')
+        author_name = commit.get('commit').get('author').get('name')
+        print(f"{sha}: {author_name}")
+
