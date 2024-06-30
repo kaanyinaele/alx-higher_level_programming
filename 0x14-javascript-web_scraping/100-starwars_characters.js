516e3fa8622bd7de579992be18196b7bf137bb60
kaanyinaele <kaanyinaele@gmail.com>
2024-06-30 11:05:21 +0100

    Who was playing in this movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,37 @@
-#!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
-  }
-});
+Write a script that prints all characters of a Star Wars movie:
+
+    The first argument is the Movie ID - example: 3 = “Return of the Jedi”
+    Display one character name by line
+    You must use the Star wars API
+    You must use the module request
+
+guillaume@ubuntu:~/0x14$ ./100-starwars_characters.js 3
+Darth Vader
+R2-D2
+Luke Skywalker
+Han Solo
+Leia Organa
+Chewbacca
+Palpatine
+Obi-Wan Kenobi
+Jabba Desilijic Tiure
+Wedge Antilles
+Yoda
+Boba Fett
+Ackbar
+Arvel Crynyd
+Mon Mothma
+Nien Nunb
+Wicket Systri Warrick
+Bib Fortuna
+C-3PO
+Lando Calrissian
+guillaume@ubuntu:~/0x14$ 
+
+Repo:
+
+    GitHub repository: alx-higher_level_programming
+    Directory: 0x14-javascript-web_scraping
+    File: 100-starwars_characters.js
+
+
